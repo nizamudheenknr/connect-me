8757a02a553bfdcb9c1822c90573402fbc815251
nizamudheenknr <nizamudheenknr@gmail.com>
2024-10-22 13:34:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div
+        className="flex justify-center items-center bg-black text-white h-screen w-screen bg-no-repeat bg-cover"
+        style={{
+          backgroundImage: "url('https://www.wscubetech.com/blog/wp-content/uploads/2024/04/social-media-1024x683.webp')"
+        }}
+      >
+        <button className='text-3xl font-semibold   hover:text-green-500 bg-blue-400 rounded-3xl p-5 hover:bg-blue-800'>connect me</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
